c3d55ef950bafd3e37a5d4470806f5f2670a0b0f
212391750 <barry.dawson@ge.com>
2019-02-15 23:43:18 -0500

    m

--- mac_changer.py
+++ mac_changer.py
@@ -3,5 +3,5 @@
 import subprocess
 
 subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66" shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
 subprocess.call("ifconfig wlan0 up", shell=True)
