661b61372366db5616f096f18c8b801826259959
212391750 <barry.dawson@ge.com>
2019-02-16 01:34:46 -0500

    added get_arg function

--- mac_changer.py
+++ mac_changer.py
@@ -3,18 +3,18 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Specify new MAC address to set")
+    (options, arguments) = parser.parse_args()
+
+    return(options, arguments)
+    
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-
-
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="Specify new MAC address to set")
-
-(options, arguments) = parser.parse_args()
 
 change_mac(options.interface, options.new_mac)
