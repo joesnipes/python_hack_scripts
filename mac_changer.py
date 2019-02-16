ca3e6d1be80ffbee12250fb535c66ddcecc493f0
212391750 <barry.dawson@ge.com>
2019-02-16 00:29:35 -0500

    m

--- mac_changer.py
+++ mac_changer.py
@@ -8,6 +8,10 @@ new_mac = input("Specify the new MAC address: ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+subprocess.call(["ifconfig ", interface, " down"])
+subprocess.call(["ifconfig ", interface, " hw ether " new_mac])
+subprocess.call(["ifconfig ", interface, " up"])
